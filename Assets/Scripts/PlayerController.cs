916722a7e8515042c0c9720731ce71e391b25d27
schmevie <ebarriga15@gmail.com>
2016-05-25 14:55:02 -0700

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float playerSpeed = 5.0f;
+	private string currentDirection = "None";
+	private Vector3 moveDirection = new Vector3 (0, 0, 0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			moveDirection = Vector3.left;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			moveDirection = Vector3.right;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			moveDirection = Vector3.up;
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			moveDirection = Vector3.down;
+		}
+		transform.position += moveDirection * playerSpeed * Time.deltaTime;
+	}
+}
